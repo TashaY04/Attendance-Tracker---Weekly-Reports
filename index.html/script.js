3565b9e70a8c7cc9802109cbd460aac0b67ce337
TashaY04 <mustitanishayadav@gmail.com>
2025-03-05 00:36:52 +0530

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,19 @@
+body {
+    font-family: Arial, sans-serif;
+}
+
+.container {
+    max-width: 800px;
+}
+
+h1 {
+    text-align: center;
+}
+
+.table {
+    margin-top: 20px;
+}
+
+.btn {
+    margin-right: 10px;
+}
